a61f3af91c9e3b9647ada2a3982f8576d3dcb68c
Kelly <kelly.hoesing@gmail.com>
2020-09-02 11:35:08 -0500

    Original Completion

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            myAccount.Deposit(int.Parse(Console.ReadLine()));
+            myAccount.GetBalance();
+        }
+    }
+}
